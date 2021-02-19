e4ce01376e2983909328aec80a68130d4e5151fb
psunil4 <sd.panchal93@gmail.com>
2021-02-18 21:33:45 -0800

    Created Header and Button components. Started working on Tasks component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header'
+import Tasks from './components/Tasks'
+import { useState } from 'react' 
+
 
 function App() {
+  const [tasks, setTasks] = useState(
+    [
+        {
+            id: 1,
+            text: 'Doctors Appointment',
+            day: 'Feb 5th at 2:30pm',
+            reminder: true
+        },
+        {
+            id: 2,
+            text: 'Order Grocery Online',
+            day: 'March 1st at 1:00pm',
+            reminder: false
+        },
+        {
+            id: 3,
+            text: 'Meeting at School',
+            day: 'Feb 22 at 4:00pm',
+            reminder: true
+        }
+    ]
+  )
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header title="Task Tracker" />
+      <Tasks tasks={tasks}/>
     </div>
   );
 }
