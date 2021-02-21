8c4bb11562b241d291b48b49453f81296ca749cc
psunil4 <sd.panchal93@gmail.com>
2021-02-20 17:44:01 -0800

    Created task component

--- src/App.js
+++ src/App.js
@@ -1,36 +1,41 @@
 import Header from './components/Header'
 import Tasks from './components/Tasks'
-import { useState } from 'react' 
+import { useState } from 'react'
 
 
 function App() {
   const [tasks, setTasks] = useState(
     [
-        {
-            id: 1,
-            text: 'Doctors Appointment',
-            day: 'Feb 5th at 2:30pm',
-            reminder: true
-        },
-        {
-            id: 2,
-            text: 'Order Grocery Online',
-            day: 'March 1st at 1:00pm',
-            reminder: false
-        },
-        {
-            id: 3,
-            text: 'Meeting at School',
-            day: 'Feb 22 at 4:00pm',
-            reminder: true
-        }
+      {
+        id: 1,
+        text: 'Doctors Appointment',
+        day: 'Feb 5th at 2:30pm',
+        reminder: true
+      },
+      {
+        id: 2,
+        text: 'Order Grocery Online',
+        day: 'March 1st at 1:00pm',
+        reminder: false
+      },
+      {
+        id: 3,
+        text: 'Meeting at School',
+        day: 'Feb 22 at 4:00pm',
+        reminder: true
+      }
     ]
   )
+
+  //Delete Task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
 
   return (
     <div className="container">
       <Header title="Task Tracker" />
-      <Tasks tasks={tasks}/>
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask}/> : 'No Tasks To Show'}
     </div>
   );
 }
